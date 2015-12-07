dcbd25f8707205c194db19884c540e99090e2901
daniel-llach <daniel.llach@gmail.com>
2015-12-07 18:13:11 -0300

    220 ms

--- LargestPalindromeProduct/largest.js
+++ LargestPalindromeProduct/largest.js
@@ -5,30 +5,31 @@ var start = new Date().getMilliseconds();
 largestPalindrome(); // digits number of each pair of products
 
 function largestPalindrome(){
-
+  var palindromes = [];
   // generate palindromes of 6 digits highest to lowest
-  for( i=1000; i>0; i--){
-    var num = i.toString();
-    var reverseNum = num.split('').reverse().join('');
-    var n = i + reverseNum;
+  for( i=999; i>0; i--){
 
     // check if is product of 2 3-digit numbers
     for( z = i; z>0 ; z-- ){
-      if( n % z === 0){
-        var digit1 = z;
-        var digit2 = n/z;
-        digit1 = digit1.toString().split('');
-        digit2 = digit2.toString().split('');
-        console.log("digits: ", digit1.length, digit2.length);
-        if( digit1.length == digit2.length ){
-          console.log("result: ", n);
-          return;
-        }
-      }
+      var temp = i*z;
+      isPalindrome(temp);
     }
   }
-}
 
+  function isPalindrome(temp){
+    var num = temp.toString();
+    var num_array = num.split('')
+
+    var start = num_array[0]+num_array[1]+num_array[2];
+    var end = num_array[5]+num_array[4]+num_array[3];
+
+    if(start==end){
+      palindromes.push(temp);
+    }
+  }
+  console.log( "result: ", Math.max.apply(null, palindromes) );
+  return;
+}
 
 var end = new Date().getMilliseconds();
 var time = end - start;
