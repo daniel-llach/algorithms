429c072c162c3edaa69a49e929180905fa53962a
daniel-llach <daniel.llach@gmail.com>
2015-12-07 16:08:31 -0300

    with execution time

--- largestPrimeFactor/largest.js
+++ largestPrimeFactor/largest.js
@@ -1,18 +1,27 @@
 // What is the largest prime factor of the number 600851475143 ?
+var start = new Date().getMilliseconds();
 largestPrimeFactor(600851475143);
+
 
 function largestPrimeFactor(num){
   var factors = [];
   i = 0;
 
-  while (i < num) {
+  do{
     if(num % i == 0 && i > 1){
       factors.push(num/i);
       num = num/i;
-    }else if(i == num-1){
-      console.log("result: ", factors[factors.length-1]);
-      return;
     }
     i++;
   }
+
+  while(i < num){
+    console.log("result: ", factors[factors.length-1]);
+    return;
+  }
 }
+
+
+var end = new Date().getMilliseconds();
+var time = end - start;
+console.log('Execution time: ' + time + ' ms.');
